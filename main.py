9657216338316c177cf7039bb4543e3d6c9260c5
venkatpesala <101573319+venkatpesala@users.noreply.github.com>
2024-01-21 11:03:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from trading_stratagy import run_trading_startagies
+from Trading_PE_CE import run_trading_startagies_PE_CE
 import os
 
 # Use the Heroku-assigned port or default to 5000
@@ -15,19 +16,20 @@ def main():
     user_tokens = st.text_area("Enter User Tokens (comma-separated)", help="Example: token1, token2")
 
         # Main Content
-    st.header("Trading Parameters")
+    # st.header("Trading Parameters")
 
     # Radio Button for Momentum Trading
-    momentum_trading_enabled = st.checkbox("Enable Momentum Trading", value=True)
-    nine_twenty_trading_enabled = st.checkbox("9:20 AM Starategy", value=True)
+    # momentum_trading_enabled = st.checkbox("Enable Momentum Trading", value=True)
+    # nine_twenty_trading_enabled = st.checkbox("9:20 AM Starategy", value=True)
 
     # Risk and Quantity Inputs
-    st.subheader("Risk and Quantity Configuration")
+    # st.subheader("Risk and Quantity Configuration")
+
 
 
     # Input for % Risk and Quantity
-    risk_percentage = st.number_input("Enter % Risk", min_value=0.1, max_value=100.0, step=0.1, value=1.0)
-    quantity = st.number_input("Enter Quantity", min_value=15, step=15, value=15)
+    # risk_percentage = st.number_input("Enter % Risk", min_value=0.1, max_value=100.0, step=0.1, value=1.0)
+    # quantity = st.number_input("Enter Quantity", min_value=15, step=15, value=15)
 
     # Button to trigger trading
     if st.button("Start Trading"):
@@ -36,12 +38,13 @@ def main():
 
         # Call your trading function with the inputs
         st_instance = st
-        run_trading_startagies(user_tokens_list, risk_percentage, quantity,momentum_trading_enabled,st_instance)
+
+        
+        message_container = st.empty()
+        # run_trading_startagies(user_tokens_list, risk_percentage, quantity,momentum_trading_enabled,st_instance)
+        # run_trading_startagies_PE_CE(user_tokens_list, risk_percentage, quantity,momentum_trading_enabled,st_instance,message_container)
+        run_trading_startagies_PE_CE(user_tokens_list, 7, 30,True,st_instance,message_container)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
